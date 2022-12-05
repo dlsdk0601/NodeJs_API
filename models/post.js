dff9a7701c1dd12478b674c1f602d13d6b66a7ac
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-05 19:29:12 +0900

    feat: 모델 생성

---
+++ models/post.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: Object,
+      required: true,
+    },
+  },
+  { timestamps: true }, // 생성 및 업데이트 시간을 체크 할 수 있음
+);
+
+export default mongoose.model("Post", postSchema);
