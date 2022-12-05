fc8224ba09f186ec688196a653b7afec0206b2c8
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-05 13:02:25 +0900

    feat: getPosts, createPost controller 생성

--- contorllers/feed.js
+++ contorllers/feed.js
@@ -1,8 +1,25 @@
-
 const getPosts = (req, res) => {
+  return res.status(200).json({
+    posts: [
+      {
+        title: "First Post",
+        content: "This is the first post!",
+      },
+    ],
+  });
+};
 
-}
+const createPost = (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.status(201).json({
+    message: "Post created Successfully",
+    post: { id: new Date().toISOString(), title, content },
+  });
+};
 
 export default {
-  getPosts
-}
+  getPosts,
+  createPost,
+};
