05d9b2f91a05d10fecda33de55792b9bd2e090a2
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-05 19:09:25 +0900

    feat: validation  최소 문자 길이 추가

--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { body } from "express-validator";
 import feedController from "../contorllers/feed.js";
 
 const router = express.Router();
@@ -6,6 +7,10 @@ const router = express.Router();
 // /feed/posts
 router.get("/posts", feedController.getPosts);
 
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [body("title").trim().isLength({ min: 7 }), body("content").trim().isLength({ min: 5 })],
+  feedController.createPost,
+);
 
 export default router;
