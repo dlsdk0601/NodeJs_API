568027fa5d1181cc9a66d771712de0c6d773db1c
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-06 23:33:45 +0900

    refactor: getPosts, createPost mongoose로 리펙토링

--- contorllers/feed.js
+++ contorllers/feed.js
@@ -1,44 +1,87 @@
 import { validationResult } from "express-validator";
+import PostModel from "../models/post.js";
 
-const getPosts = (req, res) => {
-  return res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/dummy.png",
-        creator: {
-          name: "Ina",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+const getPosts = (req, res, next) => {
+  PostModel.find()
+    .then((posts) => {
+      return res.status(200).json({
+        posts,
+        message: "Success!",
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
-const createPost = (req, res) => {
+const createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ message: "Validation fail", errors: errors.array() });
+    const error = new Error("Validation fail");
+    error.statusCode = 422;
+
+    // throw 해버리면 자동으로 함수를 멈추고 express에서 제공하는 다음 오류 처리 함수나 미들웨어로 향하려 한다.
+    throw error;
+  }
+  console.log(req.file);
+  if (!req.file) {
+    const error = new Error("No Image provided.");
+    error.statusCode = 422;
+    throw error;
   }
 
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
-
-  res.status(201).json({
-    message: "Post created Successfully",
-    post: {
-      _id: new Date().toISOString(),
-      title,
-      content,
-      creator: { name: "ina" },
-      createAt: new Date(),
-    },
+  const post = new PostModel({
+    title,
+    content,
+    imageUrl,
+    creator: { name: "ina" },
   });
+
+  post
+    .save()
+    .then((result) => {
+      return res.status(201).json({
+        message: "Post created Successfully",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  PostModel.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 400;
+        throw error;
+      }
+
+      return res.status(200).json({ message: "Post fetched", post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 export default {
   getPosts,
   createPost,
+  getPost,
 };
