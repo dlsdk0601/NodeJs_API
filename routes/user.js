bc19641108d2f6e50421bfd20d683bba93719a7e
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-07 15:29:19 +0900

    refactor: 회원가입 유효성 검사 추가, 로그인 route 생성

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,30 @@
 import express from "express";
+import { body } from "express-validator";
+import User from "../models/user.js";
+import authController from "../contorllers/auth.js";
 
 const router = express.Router();
 
-router.put("/sign-up");
+router.put(
+  "/sign-up",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (user) {
+            return Promise.reject("Email address already exists");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 5 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authController.signUp,
+);
+
+router.post("/login", authController.login);
 
 export default router;
