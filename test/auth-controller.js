a59434bed7c850fdcfada775a963948b46e55fa5
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-17 19:10:16 +0900

    refactor: before, after 사용해서 중복 코드 hook으로 정의

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -7,7 +7,29 @@ import AuthController from "../contorllers/auth.js";
 
 dotenv.config();
 
-describe("Auth Controller - Login", function (done) {
+describe("Auth Controller", function (done) {
+  // 매 테스트를 진행할때마다 연결하고 유저 만들지 말고 before로 테스트 직전에 한번만 실행
+  before((done) => {
+    mongoose
+      .connect(
+        `mongodb+srv://dlsdk0601:${process.env.MONGODB_PASSWORD}@portfolio.dacwcma.mongodb.net/test-test-api`,
+      )
+      .then((result) => {
+        const user = new User({
+          email: "test@test.com",
+          password: "tester",
+          name: "Test",
+          posts: [],
+          _id: "5c0f66b979af55031b34728a",
+        });
+
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
   it("should throw an error if accessing the database fails", function () {
     sinon.stub(User, "findOne");
     User.findOne.throws();
@@ -29,46 +51,33 @@ describe("Auth Controller - Login", function (done) {
   });
 
   it("sould send a response with a valid iser status for an existing user", (done) => {
-    mongoose
-      .connect(
-        `mongodb+srv://dlsdk0601:${process.env.MONGODB_PASSWORD}@portfolio.dacwcma.mongodb.net/test-test-api`,
-      )
-      .then((result) => {
-        const user = new User({
-          email: "test@test.com",
-          password: "tester",
-          name: "Test",
-          posts: [],
-          _id: "5c0f66b979af55031b34728a",
-        });
+    const req = { userId: "5c0f66b979af55031b34728a" };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status;
+      },
+    };
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.userStatus).to.be.equal("I'm new!");
+      done();
+    });
+  });
 
-        return user.save();
+  // 모든 테스트가 완료되고 나서 실행
+  after((done) => {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
       })
       .then(() => {
-        const req = { userId: "5c0f66b979af55031b34728a" };
-        const res = {
-          statusCode: 500,
-          userStatus: null,
-          status: function (code) {
-            this.statusCode = code;
-            return this;
-          },
-          json: function (data) {
-            this.userStatus = data.status;
-          },
-        };
-        AuthController.getUserStatus(req, res, () => {}).then(() => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.userStatus).to.be.equal("I'm new!");
-          User.deleteMany({})
-            .then(() => {
-              return mongoose.disconnect();
-            })
-            .then(() => {
-              done();
-            });
-        });
-      })
-      .catch((err) => console.log(err));
+        done();
+      });
   });
 });
