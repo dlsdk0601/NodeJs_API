c3c7be61bf46789ee6685cd7d115a38df11b15af
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-17 19:04:43 +0900

    feat: 더미 유저 생성

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,7 +1,11 @@
 import { expect } from "chai";
 import sinon from "sinon";
+import dotenv from "dotenv";
+import mongoose, { mongo } from "mongoose";
 import User from "../models/user.js";
 import AuthController from "../contorllers/auth.js";
+
+dotenv.config();
 
 describe("Auth Controller - Login", function (done) {
   it("should throw an error if accessing the database fails", function () {
@@ -22,5 +26,49 @@ describe("Auth Controller - Login", function (done) {
     });
 
     User.findOne.restore();
+  });
+
+  it("sould send a response with a valid iser status for an existing user", (done) => {
+    mongoose
+      .connect(
+        `mongodb+srv://dlsdk0601:${process.env.MONGODB_PASSWORD}@portfolio.dacwcma.mongodb.net/test-test-api`,
+      )
+      .then((result) => {
+        const user = new User({
+          email: "test@test.com",
+          password: "tester",
+          name: "Test",
+          posts: [],
+          _id: "5c0f66b979af55031b34728a",
+        });
+
+        return user.save();
+      })
+      .then(() => {
+        const req = { userId: "5c0f66b979af55031b34728a" };
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function (code) {
+            this.statusCode = code;
+            return this;
+          },
+          json: function (data) {
+            this.userStatus = data.status;
+          },
+        };
+        AuthController.getUserStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal("I'm new!");
+          User.deleteMany({})
+            .then(() => {
+              return mongoose.disconnect();
+            })
+            .then(() => {
+              done();
+            });
+        });
+      })
+      .catch((err) => console.log(err));
   });
 });
